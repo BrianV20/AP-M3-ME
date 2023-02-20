c54b0b45cc0a81c33289cbca84793c2fe7bce403
Bishor <84746849+Peteribi73@users.noreply.github.com>
2023-02-19 23:11:23 -0300

    arreglado lo de los editIcons. Sigue el javascript de estos

--- app/components/header/header.component.ts
+++ app/components/header/header.component.ts
@@ -1,3 +1,4 @@
+import { HtmlParser } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
@@ -10,17 +11,49 @@ import { Router } from '@angular/router';
 
 export class HeaderComponent implements OnInit {
   loginForm!: FormGroup;
+  
   constructor(private router: Router) { 
       /* submitBtn?.addEventListener("click", function() {
         router.navigateByUrl('/LoggedView');
       }, false); */
   }
-  /* submitBtn = document.getElementById('submitBtn');
+  submitBtn = document.getElementById('submitBtn');
   loginFunction() {
-    this.router.navigateByUrl('/LoggedView');
-  } */
+    // this.router.navigateByUrl('/LoggedView');
+
+    // SHOW EDIT ICONS
+    const icons = document.getElementsByClassName('editIcon');
+
+    for(let i = 0; i < icons.length; i++) {
+      const icon = icons[i] as HTMLElement;
+      icon.style.display = 'flex';
+
+      if(i % 2 == 0) {
+        icon.style.color = '#212529';
+      }
+      else {
+        icon.style.color = '#edf2f4';
+      }
+    }
+      
+      // FIX "DEFAULT" CSS
+    var Needsfix = document.getElementById('containerUserH3');
+    var fix = Needsfix as HTMLElement;
+    fix.style.paddingTop = '2.3em';
+
+    Needsfix = document.getElementById('expDiv');
+    fix = Needsfix as HTMLElement;
+    fix.style.paddingTop = '14em';
+  }
 
   ngOnInit(): void {
+    const editIcons = document.getElementsByClassName('editIcon');
+    for (let i = 0; i < editIcons.length; i++) {
+      const icon = editIcons[i] as HTMLElement;
+      icon.style.display = 'none';
+    }
+
+
     this.loginForm = new FormGroup({
       email: new FormControl('', [Validators.required, Validators.email]),
       password: new FormControl('', [Validators.required,  Validators.minLength(5)],)
